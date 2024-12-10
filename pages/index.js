61b197a2d94bd56addf706fbda1ba2e162aedb04
mxplayermovies <ronalddsouza888@gmail.com>
2024-12-10 06:09:31 +0530

    true

--- pages/index.js
+++ pages/index.js
@@ -608,25 +608,117 @@ const soap2daySchema = JSON.stringify({
 export default function HomePage({ allData }) {
   return (
     <>
-      <Head>
-        <title>Movies & Tv Shows™ - Online. Stream. Download.</title>
-        <link
-          rel="sitemap"
-          type="application/xml"
-          title="Sitemap"
-          href="https://moviesandtvshows.vercel.app/sitemap.xml"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Stream HD movies and TV series for free on Movies & Tv Shows™. Explore, stream, and download full-length movies and shows in HD quality without registration."
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: soap2daySchema }}
-        />
-      </Head>
+       <Head>
+ <title> Movies & Tv Shows™ - Online. Stream. Download.</title>
+
+ <link
+    rel="sitemap"
+    type="application/xml"
+    title="Sitemap"
+    href="https://moviesandtvshows.vercel.app/sitemap.xml"
+  />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/apple-touch-icon.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicon-32x32.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicon-16x16.png"
+  />
+  <link rel="manifest" href="/site.webmanifest" />
+  <meta name="googlebot" content="index,follow" />
+  <meta name="revisit-after" content="1 days" />
+  <m eta name="referrer" content="origin" />
+  <meta
+    name="robots"
+    content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+  />
+  <meta name="keywords" content="moviefree, movie free 2024, free movie, free tv shows, watch movie online, free movies online, free movie streaming, movie free streaming, download free" />
+  <meta
+    property="og:description"
+    content="Stream HD movies and TV series for free on Movies & Tv Shows™. Explore, stream, and download full-length movies and shows in HD quality without registration."
+  />
+  <meta
+    name="description"
+    content="Stream HD movies and TV series for free on Movies & Tv Shows™. Explore, stream, and download full-length movies and shows in HD quality without registration."
+  />
+  <link rel="canonical" href="https://moviesandtvshows.vercel.app/" />
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:type" content="website" />
+  <meta
+    property="og:title"
+    content=" Movies & Tv Shows™ - Online. Stream. Download. "
+  />
+  <meta property="og:url" content="https://moviesandtvshows.vercel.app" />
+  <meta
+    property="og:site_name"
+    content=" Movies & Tv Shows™ - Online. Stream. Download. "
+  />
+  <meta
+    property="og:image"
+    content="https://moviesandtvshows.vercel.app/og_image.jpg"
+  />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+  <meta property="og:image:type" content="image/jpg" />
+  <meta
+    name="application-name"
+    content=" Movies & Tv Shows™ - Online. Stream. Download. "
+  />
+  <meta
+    property="article:modified_time"
+    content="2024-01-01T13:13:13+00:00"
+  />
+  <link
+    rel="sitemap"
+    type="application/xml"
+    title="Sitemap"
+    href="https://moviesandtvshows.vercel.app/sitemap.xml"
+  />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta
+    name="twitter:title"
+    content=" Movies & Tv Shows™ - Online. Stream. Download."
+  />
+  <meta
+    name="twitter:description"
+    content="Stream HD movies and TV series for free on Movies & Tv Shows™. Explore, stream, and download full-length movies and shows in HD quality without registration."
+  />
+  <meta
+    name="twitter:image"
+    content="https://moviesandtvshows.vercel.app/og_image.jpg"
+  />
+  <meta
+    name="google-site-verification"
+    content="RNN2teFhD-lV1TQ9qcLQiSO5BLBB4DmztyYJS6QLqDg"
+  />
+
+  <meta
+    name="facebook-domain-verification"
+    content="du918bycikmo1jw78wcl9ih6ziphd7"
+  />
+  <meta
+    name="dailymotion-domain-verification"
+    content="dm3bs67ukdegz9qik"
+  />
+  <meta name="monetag" content="98a412cb5612b9188cd76b9744304b6c" />
+
+  <script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{ __html: soap2daySchema }}
+/>
+</Head>
       <SocialSharing />
       <div className="container mx-auto px-4 py-6">
         <header className="hero text-center bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 text-white p-10">
